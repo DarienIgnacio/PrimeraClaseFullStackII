71f33c8e2ca3b9e0c91fa9fa6b9b3968a21a37f2
DarienIgnacio <dari.urrutia@duocuc.cl>
2026-08-28 15:17:05 -0400

    Primera clase de js
    Aplicación de un par de herramientas de js cómo if, constantes, variables, funciones, listas, etc.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,68 @@
-const botonBienvenida = document.querySelector("#boton-bienvenida");
+//Constantes y variables
+const nombreActividad = "Taller de fotografía";
+const capacidad = 30;
+let inscritos = 18;
+const cuposDisponibles = capacidad - inscritos;
+let estado = "Sin determinar";
 
-if (botonBienvenida) {
-  botonBienvenida.addEventListener("click", () => {
-    alert("Conecta Cultura se construye paso a paso.");
-  });
+//Lo que se muestra por pantalla
+console.log(nombreActividad);
+console.log(capacidad);
+console.log(inscritos);
+console.log( `Quedan ${cuposDisponibles} cupos disponibles`);
+
+// If y else
+if (cuposDisponibles === 0) {
+  estado = "Completa";
+} else if (cuposDisponibles <= 5) {
+  estado = "Últimos cupos";
+} else {
+  estado = "Disponible";
+}
+console.log(estado);
+
+//Ejemplo funcion
+function calcularCupos(capacidad, inscritos) {
+  const disponibles = capacidad - inscritos;
+  return disponibles;
+}
+
+// Constante + llamado de funcion 
+const cuposTaller = calcularCupos(30, 18);
+console.log(cuposTaller);
+
+//Funcion + if
+function obtenerEstado(cuposDisponibles) {
+  if (cuposDisponibles === 0) {
+    return "Completa";
+  }
+
+  if (cuposDisponibles <= 5) {
+    return "Últimos cupos";
+  }
+
+  return "Disponible";
+}
+
+// Constante + funcion
+const estadoTaller = obtenerEstado(cuposTaller);
+console.log(estadoTaller);
+
+// Constante con una lista
+const actividades = [
+  "Taller de fotografía",
+  "Concierto comunitario",
+  "Muestra de teatro"
+];
+
+//Posision numerica de cada valor de la lista
+console.log(actividades[0]);
+console.log(actividades[1]);
+
+//Uso de length para calcular el tamaño de la lista (3)
+console.log(`La agenda contiene ${actividades.length} actividades`);
+
+//Uso de un for
+for (const actividad of actividades) {
+  console.log(actividad);
 }
