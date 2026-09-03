cea1c6fc99a263c903515feb4abe4914e9859620
DarienIgnacio <dari.urrutia@duocuc.cl>
2026-09-03 14:20:24 -0400

    Clase de js
    Implementación de java, funciones, ciclos para agregar a la pagina inicial

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-//Constantes y variables
+/* Constantes y variables
 const nombreActividad = "Taller de fotografía";
 const capacidad = 30;
 let inscritos = 18;
@@ -66,3 +66,82 @@ console.log(`La agenda contiene ${actividades.length} actividades`);
 for (const actividad of actividades) {
   console.log(actividad);
 }
+
+const titulo = document.querySelector("#titulo-cartelera");
+titulo.classList.add("destacado");
+
+//Cartelera
+const cartelera = document.querySelector("#cartelera");
+
+const tarjeta = document.createElement("article");
+tarjeta.classList.add("tarjeta");
+
+const nombre = document.createElement("h2");
+nombre.textContent = actividades[0].nombre;
+
+tarjeta.appendChild(nombre);
+cartelera.appendChild(tarjeta);
+*/ 
+
+const titulo = document.querySelector("#titulo-cartelera");
+const boton = document.querySelector("#boton-destacar");
+const actividades = [
+  {
+    codigo: "MUS001",
+    nombre: "Taller de guitarra inicial",
+    categoria: "Música",
+    precio: 15000,
+    cupos: 20
+  },
+  {
+    codigo: "ART002",
+    nombre: "Acuarela para principiantes",
+    categoria: "Artes visuales",
+    precio: 12000,
+    cupos: 4
+  },
+  {
+    codigo: "TEA003",
+    nombre: "Teatro comunitario",
+    categoria: "Teatro",
+    precio: 0,
+    cupos: 0
+  }
+];
+
+//FUN
+function destacarTitulo() {
+  titulo.classList.add("destacado");
+}
+boton.addEventListener("click", destacarTitulo);
+
+
+const cartelera = document.querySelector("#cartelera");
+
+//FUN
+function crearTarjeta(actividad) {
+  const tarjeta = document.createElement("article");
+  tarjeta.classList.add("tarjeta");
+
+  const nombre = document.createElement("h2");
+  nombre.textContent = actividad.nombre;
+
+  const categoria = document.createElement("p");
+  categoria.textContent = `Categoría: ${actividad.categoria}`;
+
+  const cupos = document.createElement("p");
+  cupos.textContent = `Cupos: ${actividad.cupos}`;
+
+  tarjeta.appendChild(nombre);
+  tarjeta.appendChild(categoria);
+  tarjeta.appendChild(cupos);
+  cartelera.appendChild(tarjeta);
+}
+
+//Ciclo
+for (const actividad of actividades) {
+  crearTarjeta(actividad);
+}
+
+
+
